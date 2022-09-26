6369696d9fe95ac99ee72254b1f17ef3a2eb86b6
emanueldelgadoU <emanueldelgado.u@gmail.com>
2022-09-26 20:04:39 +0200

    un cambio

---
+++ tema1/EjerciciosClase.php
@@ -0,0 +1,30 @@
+<?php
+
+
+    //hacer una tabla 5x3
+
+
+
+   
+
+    $numero=6;
+    for($i=1; $i<=10; $i++){
+        echo $numero."*".$i."=".($numero*$i);
+        echo"<br/>";
+    }
+
+ 
+
+
+// pintar caadena al reves
+
+$cadena="En un lugar de la mancha";
+
+for($i=strlen($cadena)-1; $i>=0; $i--){
+    echo $cadena[$i];
+}
+
+rand(min,max);
+
+//raiz cuadrada es 0 o negativo controlar el error
+?>
